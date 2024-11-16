328413798058fa731278c34a507c48b1482273ab
shiweifu <shiweifu@gmail.com>
2024-11-16 21:11:43 +0800

    feat:增加计时器布局

--- app/(main)/(tabs)/home.jsx
+++ app/(main)/(tabs)/home.jsx
@@ -1,26 +1,57 @@
 import { Link } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      {/* 跳转到 demo 页 */}
-      <Link href="/demo" asChild>
-        <Text>Go to Demo Page</Text>
-      </Link>
+    <View>
+      <View>
+        <View
+          className="h-40 bg-yellow-100 flex items-center justify-center 
+         text-[24px]"
+        >
+          <View className="flex-row items-center">
+            <Text className="text-[24px] font-semibold">⏱</Text>
+            <Text className="text-[24px]"> ️0m</Text>
+          </View>
+          <View className="flex-row items-center justify-center text-lg mt-2 ">
+            <Text className="text-[24px]">🍅 ✖</Text>
+            <Text className="text-[24px]  font-semibold ml-1">0</Text>
+          </View>
+        </View>
 
-      <Link href="/review" asChild>
-        <Text>Go to Demo Page</Text>
-      </Link>
+        <View>
+          <FlatList
+            data={[
+              {
+                id: 1,
+                title: "First Item",
+              },
+            ]}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View className=" bg-white flex-row justify-between p-4 items-center">
+                {/* left */}
+                <View>
+                  <View className=" flex-row items-center justify-center">
+                    <Text className="text-[20px]">🍅</Text>
+                    <Text className="text-[20px] ml-2">专注</Text>
+                    <Text className="text-[16px] leading-none ml-4">25m</Text>
+                  </View>
+
+                  <View className="mt-2">
+                    <Text>今日未执行</Text>
+                  </View>
+                </View>
+
+                {/* right */}
+                <View className=" bg-black w-8 h-8">
+                  <Text>▶</Text>
+                </View>
+              </View>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});

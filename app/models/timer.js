0a3be1531efa97bf99ea01d2f05c7517357f88cd
shiweifu <shiweifu@gmail.com>
2024-11-20 16:44:36 +0800

    feat:增加模型文件

--- app/models/timer.js
+++ app/models/timer.js
@@ -1,3 +1,21 @@
-class TimerModel {}
+class TimerModel {
+  // Constructor
+  constructor({ id, title, duration, order, type, createdAt }) {
+    this.id = id;
+    this.title = title;
+    this.duration = duration;
+    this.order = order;
+    this.type = type;
+    this.createdAt = createdAt;
+  }
+
+  get minutes() {
+    return Math.floor(this.duration / 60);
+  }
+
+  get seconds() {
+    return this.duration % 60;
+  }
+}
 
 export default TimerModel;
--- app/models/tomato.js
+++ app/models/tomato.js
@@ -1,3 +1,46 @@
-class TomatoModel {}
+const TomatoTypeBreak = 1001;
+const TomatoTypeTimer = 1002;
+
+class TomatoModel {
+  constructor({ id, title, duration, type, timerId, createdAt }) {
+    this.id = id;
+    this.title = title;
+    this.duration = duration;
+    this.type = type;
+    this.timerId = timerId;
+    this.createdAt = createdAt;
+  }
+
+  get tomatoType() {
+    if (this.type === TomatoTypeBreak) {
+      return "休息";
+    } else {
+      return "专注";
+    }
+  }
+
+  get tomatoTypeStr() {
+    if (this.type === TomatoTypeBreak) {
+      return "休息";
+    } else {
+      return "专注";
+    }
+  }
+
+  get tomatoDurationStr() {
+    const minutes = Math.floor(this.duration / 60);
+    const remainingSeconds = this.duration % 60;
+    return `${minutes.toString().padStart(2, "0")}:${remainingSeconds
+      .toString()
+      .padStart(2, "0")}`;
+  }
+
+  get createdAtStr() {
+    if (!this.createdAt) {
+      return "";
+    }
+    return `${this.createdAt.getFullYear()}-${this.createdAt.getMonth()}-${this.createdAt.getDate()}`;
+  }
+}
 
 export default TomatoModel;

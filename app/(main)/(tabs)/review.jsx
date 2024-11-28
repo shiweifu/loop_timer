6645c0cb7f9040eb6256b4853bc3d2ce42c17036
shiweifu <shiweifu@gmail.com>
2024-11-28 21:29:49 +0800

    feat:调整回顾页样式

--- app/(main)/(tabs)/review.jsx
+++ app/(main)/(tabs)/review.jsx
@@ -3,21 +3,31 @@ import { Text, View, FlatList } from "react-native";
 function ReviewPage() {
   return (
     <View>
-      {/* 列出每天的计时情况 */}
-      <FlatList
-        data={[]}
-        renderItem={({ item }) => {
-          return (
-            <View>
-              <Text>2021-09-01</Text>
-              <View>
-                <Text>🍅 ✖ 25分钟</Text>
-                <Text>⏱✖5分钟</Text>
-              </View>
-            </View>
-          );
-        }}
-      ></FlatList>
+      <View className=" min-h-screen">
+        <View className="p-4">
+          {/* 列出每天的计时情况 */}
+          <FlatList
+            numColumns={3}
+            data={["", "", "", ""]}
+            renderItem={({ item, idx }) => {
+              return (
+                <View className={"col-span-3 p-4 bg-white mt-4 mr-4"}>
+                  <Text className=" font-bold">2021-09-01</Text>
+                  <View>
+                    <View className="mt-2">
+                      <Text>🍅 ✖ 25分钟</Text>
+                    </View>
+
+                    <View className="mt-2">
+                      <Text>⏱ ✖ 5分钟</Text>
+                    </View>
+                  </View>
+                </View>
+              );
+            }}
+          ></FlatList>
+        </View>
+      </View>
     </View>
   );
 }
